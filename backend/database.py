77cb348e17adfc27329bda6291d18c8ed9df5ab4
OMAR <abnabdulaziz@outlook.com>
2026-04-19 07:30:01 +0300

    changes paths to run project

--- backend/database.py
+++ backend/database.py
@@ -3,13 +3,13 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "DATABASE_URL=postgresql://postgres:password@postgres_db:5432/srs_analyst")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:password@localhost:5432/srs_analyst")
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
