833af12751118b8c703f30164f5ec43c86b8ea17
OMAR <abnabdulaziz@outlook.com>
2026-04-19 07:12:34 +0300

    change paths to run project

--- backend/database.py
+++ backend/database.py
@@ -5,8 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:password@localhost:5432/srs_analyst")
-
+DATABASE_URL = os.getenv("DATABASE_URL", "DATABASE_URL=postgresql://postgres:password@postgres_db:5432/srs_analyst")
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
