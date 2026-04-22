d28b1cf6ac199430b4c1b6008b2fe24e552abfc5
devbjack <devbjack@gmail.com>
2026-04-22 11:04:24 +0300

    fix: removing suspense

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,8 +6,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Suspense fallback={<div className="p-4 text-center">Loading...</div>}>
-      <App />
-    </Suspense>
+    <App />
   </React.StrictMode>,
 )
